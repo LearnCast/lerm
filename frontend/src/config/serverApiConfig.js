7d87118c99454343da016ff384fcda57ae9a1d30
Eddie Villanueva <edvillan15@gmail.com>
2023-06-18 19:02:00 +0800

    Updated remote react endpoint

--- frontend/src/config/serverApiConfig.js
+++ frontend/src/config/serverApiConfig.js
@@ -1,9 +1,9 @@
 export const API_BASE_URL =
   process.env.NODE_ENV == 'production' || process.env.REACT_APP_DEV_REMOTE == 'remote'
-    ? 'https://calm-ruby-barracuda-sock.cyclic.app/api/'
+    ? 'https://lerm-express.cyclic.app/api/'
     : 'http://localhost:8888/api/';
 export const DOWNLOAD_BASE_URL =
   process.env.NODE_ENV == 'production' || process.env.REACT_APP_DEV_REMOTE == 'remote'
-    ? 'https://calm-ruby-barracuda-sock.cyclic.app/download/'
+    ? 'https://lerm-express.cyclic.app/download/'
     : 'http://localhost:8888/download/';
 export const ACCESS_TOKEN_NAME = 'x-auth-token';
